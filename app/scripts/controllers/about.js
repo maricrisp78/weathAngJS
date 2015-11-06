bec1d6977ccb38e6a1b99ae6a3e89bb2c757ed19
Marie Porciuncula <marieporciuncula@maries-air.seattleu.edu>
2015-11-05 19:19:43 -0800

    commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weathAngJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weathAngJsApp
+ */
+angular.module('weathAngJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weathAngJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weathAngJsApp
+ */
+angular.module('weathAngJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
